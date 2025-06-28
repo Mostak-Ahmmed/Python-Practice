0324884e21257b2d4977b8df08c8f2745ea660fc
Mostak <125486372+Mostak-Ahmmed@users.noreply.github.com>
2025-06-29 00:00:34 +0600

    Add files via upload

---
+++ 213902126 task1.py
@@ -0,0 +1,22 @@
+
+
+# Lab TASK # 01
+
+# Write a Python program that accepts,
+# two integers (n, m) and computes the value of n^2 + 2nm + m^2, 
+# and computes (n+m)^2, 
+# now use comparison operator to evaluate whether they are equal
+# and print the value of the comaprison.
+
+n=8
+m=4
+
+eq1=(n**2+2*n*m+m**2)
+
+eq2=((n+m)**2)
+
+print(eq1==eq2)
+
+
+
+
